f623deda1befd89e5863ff7afde519d38d997919
Chew <chew@chew.pw>
2026-01-03 16:40:52 -0600

    a bit more setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// @ts-ignore Generated after build
+import { commands, components, events } from "../.dressed";
+import { handleRequest, setupCommands, setupComponents, setupEvents } from "dressed/server";
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	fetch: (req: Request, _env: never, ctx: { waitUntil: <T>(f: T) => T }) =>
+		handleRequest(
+			req,
+			(...p) => ctx.waitUntil(setupCommands(commands)(...p)),
+			(...p) => ctx.waitUntil(setupComponents(components)(...p)),
+			(...p) => ctx.waitUntil(setupEvents(events)(...p))
+		),
+};
